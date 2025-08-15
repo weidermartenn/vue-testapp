2d0f8b669a230d475592e22eb518f9afa3e21fb1
wdrf14 <weidermartenn@gmail.com>
2025-08-15 14:49:27 +0300

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],  
-  server: {
-    allowedHosts: ['c2ec7da9d51d.ngrok-free.app']
-  }  
+  plugins: [vue()],   
 })
